db4db572ba0fe0f116805138a266946dbfa4eb7b
Yismen Jorge <yismen.jorge@gmail.com>
2016-10-25 15:31:42 -0400

    Typo

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Role;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
 
 class RoleTableSeeder extends Seeder
 {
@@ -17,17 +15,18 @@ class RoleTableSeeder extends Seeder
 
         Role::truncate();
 
-        Role::create([[
-                'id' => 1,
-                'name' => 'admin', 
-                'display_name' => 'System Administrator', 
-                'description' => 'Application super user. Users with this role has no restriction.'
-            ],
-                ['id' => 2,
-                'name' => 'owner', 
-                'display_name' => 'Application Owner', 
-                'description' => 'Application owner. Just litle restriction.'
-            ]
+        Role::create([
+            'id' => 1,
+            'name' => 'admin', 
+            'display_name' => 'System Administrator', 
+            'description' => 'Application super user. Users with this role has no restriction.'
+        ]);
+
+        Role::create([
+            'id' => 2,
+            'name' => 'owner', 
+            'display_name' => 'Application Owner', 
+            'description' => 'Application owner. Little restriction. Just to differentiate from the system admin.'
         ]);
 
         Model::reguard();
