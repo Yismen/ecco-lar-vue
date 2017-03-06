ae43ceb47770b963207156b436983c3ef7d52830
Yismen Jorge <yismen.jorge@gmail.com>
2017-03-06 01:46:06 -0400

    Updates

--- resources/views/escalations_admin/partials/links.blade.php
+++ resources/views/escalations_admin/partials/links.blade.php
@@ -1,5 +1,6 @@
 <div class="list-group">
     <a href="/admin/escalations_admin/" class="list-group-item active"><i class="fa fa-home"></i> Home</a>
+    <a href="/admin/escalations_clients/" class="list-group-item active"><i class="fa fa-home"></i> Manage Clients</a>
     <a href="/admin/escalations_admin/by_date" class="list-group-item"><i class="fa fa-angle-double-right"></i> Records By Date</a>
     <a href="#" class="list-group-item"><i class="fa fa-angle-double-right"></i> Records by Period</a>
     <a href="/admin/escalations_admin/search" class="list-group-item"><i class="fa fa-angle-double-right"></i> Search Records</a>
