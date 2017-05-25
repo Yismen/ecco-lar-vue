64b3bbb1429a4b5e55c5d09624acf3eca7aeea11
Yismen Jorge <yismen.jorge@gmail.com>
2017-05-25 12:47:02 -0400

    Afp and ARS

--- app/Ars.php
+++ app/Ars.php
@@ -3,8 +3,39 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class Ars extends Model
+class Ars extends Model implements SluggableInterface
 {
-    //
+    use SluggableTrait;
+
+    protected $table = 'ars';
+    
+    protected $sluggable = [
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    ];
+
+
+    protected $fillable = ['name'];
+
+    // Relationships =============================================
+    public function employees()
+    {
+        return $this->hasMany('App\Employee', 'ars_id');
+    }
+    
+    // Methods ===================================================
+    
+    // Scopes ====================================================
+    
+    // Accessors =================================================
+    
+    // Mutators ==================================================
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = trim(ucwords($name));
+    }
+    
 }
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,19 +4,20 @@
 @section('content')
       <div class="col-sm-8 col-sm-offset-2">
         <div class="box box-primary pad">
-          <div class="error-page">
-            <h2 class="headline text-yellow"> 404</h2>
+          <div class="error-page row">
+            <h2 class="headline text-yellow">404</h2>
 
             <div class="error-content">
               <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
 
               <p>
                 We could not find the page you were looking for.
-                Meanwhile, you may <a href="/">return to home page</a>.
+                Meanwhile, you may <a href="{{ url()->previous() }}">return to home page</a>.
               </p>
+
             </div>
             <!-- /.error-content -->
-          </div>
+          </div> {{-- /.error page --}}
         </div>
       </div>
 @endsection
