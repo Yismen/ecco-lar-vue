3e9aae944ca920c2d29b1c8c9876aa0af21be073
Yismen Jorge <yismen.jorge@gmail.com>
2017-05-26 10:29:37 -0400

    Fixed typos

--- resources/views/emails/password-reset.blade.php
+++ resources/views/emails/password-reset.blade.php
@@ -14,13 +14,13 @@
 
 @section('content')
     <p>
-        Please click here <a href="{{ url('/admin/login') }}">Dainsys</a> to log in with the new credentials, or copy and paste this link into your web browser: <br>
+        Please click here <a href="{{ url('/admin/login') }}">Dainsys</a> to log in with the new credentials, or copy and paste this link into your web browser URL: <br>
         {{ url('/admin/login') }}
         <hr>
         <h5>Credentials:</h5>
         <strong>Username:</strong> {{ $user->email }} <br>
         <strong>Password:</strong> {{ $password }} <br>
-        <small>We strongly recomend to change this password as soon as yo login!</small>
+        <small>We strongly encourage to change this password as soon as you log in!</small>
     </p>
 @stop
 
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -14,12 +14,12 @@
 
 @section('content')
     <p>
-        We have created an user for you in our Dainsys app. Please access the following link to log in: <a href="{{ url('/admin/login') }}">Dainsys</a> copy and paste the following text: <br>
+        We have created an user for you in our Dainsys app. Please access the following link to log in: <a href="{{ url('/admin/login') }}">Dainsys</a>, or copy and paste this link into your web browser URL: <br>
         {{ url('/admin/login') }}
         <hr>
         These are your credentials: <br>
         <strong>Username:</strong> {{ $user->email }} <br>
         <strong>Password:</strong> {{ $password }} <br>
-        <small>We strongly recomend to change this password as soon as yo login!</small>
+        <small>We strongly encourage you to change this password as soon as you log in!</small>
     </p>
 @stop
