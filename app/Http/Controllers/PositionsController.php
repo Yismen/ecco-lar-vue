9d59c65ba3888ba33e7de7f107cd524d3d4039f6
Yismen Jorge <yismen.jorge@gmail.com>
2017-06-08 11:12:28 -0400

    Nah

--- app/Http/Controllers/PositionsController.php
+++ app/Http/Controllers/PositionsController.php
@@ -27,9 +27,6 @@ class PositionsController extends Controller {
 			->orderBy('department_id')
 			->orderBy('name')
 			->with('department')
-			->orderBy('department_id', function($query){
-				return $query->orderBy('department');
-			})
 			->with('payment')
 			->paginate(50);
 
