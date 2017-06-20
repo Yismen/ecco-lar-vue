0a8fc9b4c3e8da6498bbbe9070c2cd7e9d990de5
Yismen Jorge <yismen.jorge@gmail.com>
2017-06-20 13:15:01 -0400

    Allowing registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,82 +1,89 @@
-@extends('layouts.app')
+@inject('layout', 'App\Layout')
+@extends('layouts.'.$layout->app(), ['page_header'=>'Register to Dainsys', 'page_description'=>'New Users Registrations Page'])
 
-@section('content') 
-      <div class="col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
-        <div class="box box-primary pad">
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                <div class="box box-primary">
+                    
+                    {!! Form::open(['url'=>['admin/register',], 'method'=>'POST', 'class'=>'', 'role'=>'form', 'autocomplete'=>"off"]) !!}    
 
-          {!! Form::open(['url'=>['admin/register',], 'method'=>'POST', 'class'=>'', 'role'=>'form', 'autocomplete'=>"off"]) !!}    
-            <div class="form-group">
-              <legend>Register as a new membership</legend>
-            </div>
+                        <div class="box-header with-border">
+                            <h3>Register as a new membership</h3>
+                        </div>
+                
+                        <div class="box-body">
+                            <div class="form-group {{ $errors->has('name') ? 'has-error' : null }}  has-feedback">  
+                                {!! Form::input('text', 'name', null, ['class'=>'form-control', 'placeholder'=>'Full Name']) !!}
+                                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                                {!! $errors->first('name', '<span class="text-danger">:message</span>') !!}
+                            </div>
+                            <!-- /. Name -->
 
-            {{-- Display Errors --}}
-            @if( $errors->any() )
-              <div class="col-sm-12">
-                <div class="alert alert-danger">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                  <ul>
-                    @foreach ($errors->all() as $error)
-                      <li>{{ $error }}</li>
-                    @endforeach
-                  </ul>
+                            <div class="form-group {{ $errors->has('email') ? 'has-error' : null }} has-feedback">
+                                {!! Form::input('text', 'email', null, ['class'=>'form-control', 'placeholder'=>'Email Address']) !!}
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                                {!! $errors->first('email', '<span class="text-danger">:message</span>') !!}
+                            </div>
+                            <!-- /. Email Address -->
+
+                            <div class="form-group {{ $errors->has('username') ? 'has-error' : null }} has-feedback">
+                                {!! Form::input('text', 'username', null, ['class'=>'form-control', 'placeholder'=>'User Name']) !!}
+                                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                                {!! $errors->first('username', '<span class="text-danger">:message</span>') !!}
+                            </div>
+                            <!-- /. User Name -->
+
+                            <div class="form-group {{ $errors->has('password') ? 'has-error' : null }} has-feedback">
+                                {!! Form::input('password', 'password', null, ['class'=>'form-control', 'placeholder'=>'Password']) !!}
+                                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                                {!! $errors->first('password', '<span class="text-danger">:message</span>') !!}
+                            </div>
+                            <!-- /. Password -->
+                            
+                            <div class="form-group {{ $errors->has('password_confirmation') ? 'has-error' : null }} has-feedback">
+                                {!! Form::input('password', 'password_confirmation', null, ['class'=>'form-control', 'placeholder'=>'Retype Password']) !!}
+                                <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                                {!! $errors->first('password_confirmation', '<span class="text-danger">:message</span>') !!}
+                            </div>
+                            <!-- /. Retype Password -->
+                            {{-- <div class="row">
+                              <div class="col-xs-8">
+                                <div class="checkbox icheck">
+                                  <label>
+                                    <input type="checkbox"> I agree to the <a href="#">terms</a>
+                                  </label>
+                                </div>
+                              </div> --}}
+                              <!-- /.col -->
+
+                        </div>
+                
+                        <div class="box-footer">
+                            <div class="col-xs-4">
+                                <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+                            </div>
+
+                            <div class="social-auth-links text-center">
+                                <p>- OR -</p>
+                                <hr>
+                                <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign up using
+                                Facebook</a>
+                                <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign up using
+                                Google+</a>
+                            </div>
+
+                                <a href="{{ url('admin/login') }}" class="text-center">I already have a membership</a>
+                        </div>
+
+                    {!! Form::close() !!}
+                    
                 </div>
-              </div>
-            @endif
-            {{-- /. Errors --}}
-
-            <div class="form-group has-feedback">
-              <input type="text" class="form-control" placeholder="Full name">
-              <span class="glyphicon glyphicon-user form-control-feedback"></span>
             </div>
-
-            <div class="form-group has-feedback">
-              <input type="email" class="form-control" placeholder="Email">
-              <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-            </div>
-
-            <div class="form-group has-feedback">
-              <input type="text" class="form-control" placeholder="Username">
-              <span class="glyphicon glyphicon-user form-control-feedback"></span>
-            </div>
-
-            <div class="form-group has-feedback">
-              <input type="password" class="form-control" placeholder="Password">
-              <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-            </div>
-
-            <div class="form-group has-feedback">
-              <input type="password" class="form-control" placeholder="Retype password">
-              <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-              <!-- <i class="fa fa-sign-in"></i> -->
-            </div>
-            <div class="row">
-              <div class="col-xs-8">
-                <div class="checkbox icheck">
-                  <label>
-                    <input type="checkbox"> I agree to the <a href="#">terms</a>
-                  </label>
-                </div>
-              </div>
-              <!-- /.col -->
-              <div class="col-xs-4">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-              </div>
-              <!-- /.col -->
-            </div>
-
-            <div class="social-auth-links text-center">
-              <p>- OR -</p>
-              <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign up using
-                Facebook</a>
-              <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign up using
-                Google+</a>
-            </div>
-
-            <a href="{{ url('admin/login') }}" class="text-center">I already have a membership</a>
-          
-          {!! Form::close() !!}
-
         </div>
-      </div>
-        <!-- /.register-box -->
+    </div>
 @endsection
+@section('scripts')
+
+@stop
