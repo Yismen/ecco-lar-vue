5c3d2d1e234de692c6edb2dca6b8ceed1a4938ee
Yismen Jorge <yismen.jorge@gmail.com>
2017-10-15 09:29:08 -0400

    Removed show and edit functions

--- app/Http/Controllers/PayrollsController.php
+++ app/Http/Controllers/PayrollsController.php
@@ -15,7 +15,7 @@ class PayrollsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function generate()
     {
         return "All reports from here...";
     }
@@ -25,18 +25,7 @@ class PayrollsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function close()
     {
         //
     }
@@ -47,30 +36,7 @@ class PayrollsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function details($id)
     {
         //
     }
