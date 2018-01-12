b699f0ff273081c5909b5ac60c4fa369d930a421
Yismen Jorge <yismen.jorge@gmail.com>
2018-01-12 16:14:46 -0400

    Changed table structure

--- resources/views/positions/index.blade.php
+++ resources/views/positions/index.blade.php
@@ -3,57 +3,63 @@
 
 @section('content')
 	<div class="container-fluid">
-		<div class="box box-primary pad">
-		
-			<h3 class="page-header">
-				Positions Items List 
-			 	<a href="{{ route('admin.positions.create') }}">
-			 		<i class="fa fa-plus"></i>
-			 	</a>
-			</h3>
+		<div class="col-sm-8 col-sm-offset-2">
+			<div class="box box-primary pad">
+			
+				<h3 class="page-header">
+					Positions Items List 
+				 	<a href="{{ route('admin.positions.create') }}">
+				 		<i class="fa fa-plus"></i>
+				 	</a>
+				</h3>
 
-			@if ($positions->isEmpty())
-				<div class="bs-callout bs-callout-warning">
-					<h1>No Positions has been added yet, please add one</h1>
-				</div>
-			@else
-				<table class="table table-condensed table-striped">
-					<thead>
-						<tr>
-							<th>Position Name</th>
-							<th>Department</th>
-							<th>Salary</th>
-							<th class="col-xs-3">								 
-								<a href="{{ route('admin.positions.create') }}">
-									Add 
-							 		<i class="fa fa-plus"></i>
-							 	</a>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach ($positions as $position)
+				@if ($positions->isEmpty())
+					<div class="bs-callout bs-callout-warning">
+						<h1>No Positions has been added yet, please add one</h1>
+					</div>
+				@else
+					<table class="table table-condensed table-striped">
+						<thead>
 							<tr>
-								<td>
-									<a href="{{ route('admin.positions.show', $position->id) }}">{{ $position->department->department }} - {{ $position->name }}</a>
-								</td>
-
-								<td>
-									<a href="{{ route('admin.departments.show', $position->department->id) }}">{{ $position->department->department }}</a>
-								</td>
-								<td>$ {{ number_format($position->salary, 2) }}</td>
-								<td>
-									<a href="{{ route('admin.positions.edit', $position->id) }}" rel="tooltip" title="Edit" data-placement="left">
-										<i class="fa fa-pencil"></i>
-									</a>
-									{{-- {!! delete_button('positions.destroy', $position->id, ['class'=>'btn btn-danger','label'=>'<i class="fa fa-trash"></i>']) !!} --}}
-								</td>
+								<th>Position Name</th>
+								<th>Department</th>
+								<th>Salary</th>
+								<th>Payment Frequency</th>
+								<th>Payment Type</th>
+								<th>								 
+									<a href="{{ route('admin.positions.create') }}">
+										Add 
+								 		<i class="fa fa-plus"></i>
+								 	</a>
+								</th>
 							</tr>
-						@endforeach
-					</tbody>
-				</table>
-				{!! $positions->render() !!}
-			@endif
+						</thead>
+						<tbody>
+							@foreach ($positions as $position)
+								<tr>
+									<td>
+										<a href="{{ route('admin.positions.show', $position->id) }}">{{ $position->department->department }} - {{ $position->name }}</a>
+									</td>
+
+									<td>
+										<a href="{{ route('admin.departments.show', $position->department->id) }}">{{ $position->department->department }}</a>
+									</td>
+									<td>$ {{ number_format($position->salary, 2) }}</td>
+									<td>{{ $position->payment_frequency ? $position->payment_frequency->name : '' }}</td>
+									<td>{{ $position->payment_type ? $position->payment_type->name : '' }}</td>
+									<td class="text-warning">
+										<a href="{{ route('admin.positions.edit', $position->id) }}" rel="tooltip" title="Edit" data-placement="left" class="text-warning">
+											<i class="fa fa-pencil"></i> Edit
+										</a>
+										{{-- {!! delete_button('positions.destroy', $position->id, ['class'=>'btn btn-danger','label'=>'<i class="fa fa-trash"></i>']) !!} --}}
+									</td>
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
+					{!! $positions->render() !!}
+				@endif
+			</div>
 		</div>
 	</div>
 @stop
