8d8fe49fef05a6b1b79a364e41ffc79ce2016731
Yismen Jorge <yismen.jorge@gmail.com>
2018-01-16 21:41:00 -0400

    Added fillable fields

--- app/TerminationReason.php
+++ app/TerminationReason.php
@@ -4,6 +4,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TerminationReason extends Model {
 
-	//
+	/**
+     * mass assignable
+     */
+    protected $fillable = ['reason', 'description'];
+
+    public function setReasonAttribute($reason)
+    {
+        return $this->attributes['reason'] = ucwords(trim($reason));
+    }
 
 }
