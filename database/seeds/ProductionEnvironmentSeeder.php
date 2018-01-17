69d1116893c5726d7bf7ee38da179a52f9576521
Yismen Jorge <yismen.jorge@gmail.com>
2018-01-17 16:21:23 -0400

    Moved users seeding to a seeder

--- database/seeds/ProductionEnvironmentSeeder.php
+++ database/seeds/ProductionEnvironmentSeeder.php
@@ -29,14 +29,13 @@ class ProductionEnvironmentSeeder extends Seeder
     protected function seedUsersTable()
     {
         // User::truncate();
-
-        User::create([
+        factory(App\User::class)->create([
             'name' => 'Yismen Jorge', // $faker->name,
             'email' => 'yismen.jorge@gmail.com', // $faker->email,
             'password' => bcrypt('password'), // bcrypt(str_random(10)),
             'remember_token' => str_random(10),
-            // 'is_active ' => 1,
-            // 'is_admin ' => 1,
+            'is_active ' => 1,
+            'is_admin ' => 1,
         ]);
 
         return $this;
