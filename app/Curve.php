5c834142f6f13f94cf1c1b0a15a0e7f9ab06e2db
Yismen Jorge <yismen.jorge@gmail.com>
2018-01-17 15:26:57 -0400

    Added mass assign

--- app/Curve.php
+++ app/Curve.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curve extends Model {
 
-	//
+	/**
+     * mass assignable
+     */
+    protected $fillable = ['days_in_production_limit', 'goal_percentage_required'];
 
 }
