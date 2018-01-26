b28366024e2a89dea1866f129d02cd2b0e4c134e
Yismen Jorge <yismen.jorge@gmail.com>
2018-01-26 14:40:03 -0400

    Removed unnecessary tag, changed title

--- resources/views/human_resources/partials/monthly_attrition.blade.php
+++ resources/views/human_resources/partials/monthly_attrition.blade.php
@@ -1,10 +1,8 @@
 <div class="box box-primary danger-bg">
-    <div class="box-header with-border"><h4>Rotation by Month This Year</h4></div>
+    <div class="box-header with-border"><h4>Monthly Attrition (Last five months)</h4></div>
 
     <div class="box-body">
         
     </div>
-
-    <div class="box-footer"></div>
     
 </div>
