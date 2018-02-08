caddd6c0a460e68cfae668287a4f12822966e203
Yismen Jorge <yismen.jorge@gmail.com>
2018-02-08 17:07:25 -0400

    Fixed route name

--- routes/Web/Auth/notes.php
+++ routes/Web/Auth/notes.php
@@ -4,6 +4,6 @@ Route::bind('note', function($slug) {
     return App\Note::whereSlug($slug)->firstOrFail();
 });
 
-Route::get('notes/trashed', ['as'=>'admin.notes.trashed', 'uses'=>'NotesAdminController@trashedNotes']);
-Route::get('notes/restore/{slug}', ['as'=>'admin.notes.restore', 'uses'=>'NotesAdminController@restoreNotes']);
+Route::get('notes/trashed', ['as'=>'notes.trashed', 'uses'=>'NotesAdminController@trashedNotes']);
+Route::get('notes/restore/{slug}', ['as'=>'notes.restore', 'uses'=>'NotesAdminController@restoreNotes']);
 Route::resource('notes', 'NotesAdminController');
