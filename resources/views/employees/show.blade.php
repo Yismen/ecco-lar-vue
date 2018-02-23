cc1e0c31ed19bc947b6d24dc4489cf410e7edd2a
Yismen Jorge <yismen.jorge@gmail.com>
2018-02-23 10:19:11 -0400

    typo...

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -90,7 +90,7 @@
 									${{ number_format($employee->position->salary, 2) }}, 
 									{!! $employee->position->payment_type->name or '<span class="text-danger">Missing Payment Type in his position. Please fix!</span>' !!}, 
 									{!! $employee->position->payment_frequency->name or '<span class="text-danger">Missing Payment Frequency in his position. Please fix!</span>' !!} 
-									<a href="{{ route('admin.positions.edit', $position->id) }}"><i class="fa fa-pencil"></i></a>
+									<a href="{{ route('admin.positions.edit', $employee->position->id) }}"><i class="fa fa-pencil"></i></a>
 								@endif
 							</li>
 						
