f92c6de39a3f79d51dc8bccd4c790cd4567a3d94
Yismen Jorge <yismen.jorge@gmail.com>
2018-02-24 19:03:42 -0400

    ...

---
+++ database/migrations/2018_02_24_172055_remove_system_id_from_logins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveSystemIdFromLoginsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn('logins', 'system_id')) {
+            Schema::disableForeignKeyConstraints();
+            Schema::table('logins', function (Blueprint $table) {
+                $table->dropForeign('logins_ibfk_2');
+                $table->dropColumn('system_id');
+            });
+
+            Schema::enableForeignKeyConstraints();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('logins', function (Blueprint $table) {
+            //
+        });
+    }
+}
