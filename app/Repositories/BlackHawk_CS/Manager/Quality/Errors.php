49ee711acd3aa45b7aa8712aa75cfe7948fff4d8
Yismen Jorge <yismen.jorge@gmail.com>
2018-06-15 13:29:50 -0400

    Pass request instance

--- app/Repositories/BlackHawk_CS/Manager/Quality/Errors.php
+++ app/Repositories/BlackHawk_CS/Manager/Quality/Errors.php
@@ -4,27 +4,16 @@ namespace App\Repositories\BlackHawk_CS\Manager\Quality;
 
 use App\Repositories\BlackHawk_CS\Manager\Quality\Errors\Monthly;
 use App\Repositories\BlackHawk_CS\Manager\Quality\Errors\Weekly;
+use Illuminate\Http\Request;
 
 class Errors
 {
     public $monthly = [];
     public $weekly = [];
 
-    private $request;
-
-    public function __construct()
+    public function __construct(Request $request)
     {
-        $this->monthly = $this->monthly();
-        $this->weekly = $this->weekly();
-    }
-
-    private function monthly()
-    {
-        return new Monthly;
-    }
-
-    private function weekly()
-    {
-        return new Weekly();
+        $this->weekly = new Weekly($request);
+        $this->monthly = new Monthly($request);
     }
 }
