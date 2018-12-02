9502d8bd26c656fe0a5ed884ffb5b19775a7905b
Yismen Jorge <yismen.jorge@gmail.com>
2018-12-02 13:41:35 -0400

    Implement Delete Button component

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -86,6 +86,7 @@ Vue.component( 'passport-authorized-clients', require('./components/passport/Aut
 Vue.component( 'passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue') );
 Vue.component('create-position', require('./components/employees/positions/Create'))
 Vue.component('add-button', require('./components/links/AddButton'))
+Vue.component('delete-request-button', require('./components/DeleteRequest'))
 Vue.component('create-afp-form', require('./components/forms/CreateAfp'))
 Vue.component('flash-message', require('./components/FlashMessage'))
 Vue.component('employee-edit', require('./components/employees/Edit'));
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,21 +3,32 @@
 
 @section('content')
   <div class="col-sm-8 col-sm-offset-2">
-    <div class="box box-primary pad">
-      <div class="error-page row">
-        <h2 class="headline text-yellow">404</h2>
+    <div class="box box-primary">
+      <div class="box-body">
+        <div class="error-page">
+          <h2 class="headline text-yellow">404</h2>
 
-        <div class="error-content">
-          <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
+          <div class="error-content">
+            <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
 
-          <p>
-            We could not find the page you were looking for.
-            Meanwhile, you may <a href="{{ url()->previous() }}">return to home page</a>.
-          </p>
+            <p>
+              This you are look for does't belong here. Keep searching somewhere else!
+              {{-- We could not find the page you were looking for.
+              Meanwhile, you may <a href="{{ url()->previous() }}">return to home page</a>. --}}
+            </p>
 
+            <p>
+              <a href="{{ url()->previous() }}" class="btn btn-default">
+                <i class="fa fa-home"></i>
+                Go Back
+              </a>
+            </p>
+
+          </div>
+          <!-- /.error-content -->
         </div>
-        <!-- /.error-content -->
-      </div> {{-- /.error page --}}
+        {{-- /.error page --}}
+      </div>
     </div>
   </div>
 @endsection
