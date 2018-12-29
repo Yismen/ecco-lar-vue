27c903cb52673acbf31043cc3cd9f50a5a319ca5
vagrant <vagrant@dainsys>
2018-12-29 14:35:12 +0000

    Refactor DainsysAuthorization

--- app/Http/Middleware/DainsysAuthorization.php
+++ app/Http/Middleware/DainsysAuthorization.php
@@ -9,28 +9,6 @@ use Illuminate\Support\Facades\Auth;
 class DainsysAuthorization
 {
     /**
-     * Laravel Request Object.
-     */
-    private $request;
-    /**
-     * Laravel Next Clousure.
-     */
-    private $next;
-    private $user;
-    /**
-     * If set to true, the current request will be rejected.
-     */
-    private $reject;
-    /**
-     * The message to be displayed when a request is rejected.
-     */
-    private $message;
-    /**
-     * Permissions object parsed from the string given by the request.
-     */
-    private $permissions;
-
-    /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -39,124 +17,35 @@ class DainsysAuthorization
      */
     public function handle($request, Closure $next, $permissions = null)
     {
-        $this->request = $request;
-        $this->next = $next;
-        $this->permissions = $permissions;
-        $this->user = Auth::user();
+        $this->guardAgainstUnauthenticated();
 
-        $this->handleAuthenthication();
-
-        if (
-            // config('app.env') == 'production' &&
-            $this->isOwnerOrAdmin()
-        ) {
+        if ($request->user()->hasAnyRole(['admin', 'owner'])) {
             return $next($request);
         }
 
-        $this->parsePermissions()
-            ->handlePermsissions();
-
-        if ($this->reject) {
-            if ($this->request->ajax()) {
-                return response($this->message, 401);
-            }
-
-            return redirect()
-                ->back()
-                ->withImportat(true)
-                ->withDanger($this->message);
-        }
+        $this->guardAgainstUnauthorizedUsers($request, $permissions);
 
         return $next($request);
     }
 
-    /**
-     * Check if the user is authenticated.
-     *
-     * @return object Current class.
-     */
-    private function handleAuthenthication()
+    protected function parsePermissions($permissions)
     {
-        if (Auth::guest()) {
-            if ($this->request->ajax() || $this->request->wantsJson()) {
-                return response('Unauthorized.', 401);
-            } else {
-                // return redirect()->guest('admin/login');
-                return redirect()->guest('login');
-            }
-        }
-
-        return $this;
+        return explode("|", $permissions);
     }
 
-    /**
-     * Checks wether the user has admin or owner roles
-     * @return boolean [description]
-     */
-    private function isOwnerOrAdmin()
+    protected function guardAgainstUnauthenticated()
     {
-        return $this->user->hasRole('admin')
-            || $this->user->hasRole('owner');
+        if (! auth()->check()) {
+            abort(401, 'Unauthenticated');
+        }
     }
 
-    private function handlePermsissions()
+    protected function guardAgainstUnauthorizedUsers($request, $permissions)
     {
-        $permissions = $this->permissions;
-        if (!$permissions) {
-            return $this;
+        if (! $request->user()->hasAnyPermission(
+            $this->parsePermissions($permissions)
+        )) {
+            abort(403, 'Unauthorized');
         }
-
-        foreach ($permissions as $permission) {
-            if ($this->user->can($permission)) {
-                return $this;
-            }
-        }
-
-        Log::error(
-            'User ' . $this->user->name . ' was denied to access route [' .
-            $this->request->path() .
-            '] because missing required permission [' . $permission . '].'
-        );
-
-        return $this->reject(
-            "Unauthorized. Permission [$permission] is required to view route " .
-            $this->request->path() . '. Contact Site Admin.'
-        );
-    }
-
-    /**
-     * Convert the permissions into an array of items.
-     * @param   string $permissions  The list of roles passed by the users
-     * @return [type]         [description]
-     */
-    private function parsePermissions()
-    {
-        $permissions = $this->permissions;
-
-        if (!$permissions) {
-            return;
-        }
-
-        $permissions = explode('|', $this->permissions);
-
-        $permissions = array_map(function ($value) {
-            return trim($value);
-        }, $permissions);
-
-        $this->permissions = $permissions;
-
-        return $this;
-    }
-
-    /**
-     * Reject the unauthorized request.
-     *
-     * @param  string $message [description]
-     * @return [type]          [description]
-     */
-    private function reject($message = 'Unauthorized.')
-    {
-        $this->message = $message;
-        return $this->reject = true;
     }
 }
