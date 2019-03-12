25517a76d3cedc410b1d80cea22f650b1b24b14a
Yismen Jorge <yismen.jorge@gmail.com>
2019-03-11 23:13:10 -0400

    Add Horizon route/icons

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -14,10 +14,11 @@ class MenusTableSeeder extends Seeder
     {
         Menu::truncate();
 
-        Menu::create(['name' => 'telescope', 'display_name' => 'Telescope', 'description' => '']);
-        Menu::create(['name' => 'admin/users', 'display_name' => 'Users', 'description' => '']);
-        Menu::create(['name' => 'admin/roles', 'display_name' => 'Roles', 'description' => '']);
-        Menu::create(['name' => 'admin/permissions', 'display_name' => 'Permissions', 'description' => '']);
+        Menu::create(['name' => 'telescope', 'display_name' => 'Telescope', 'description' => '', 'icon' => 'fa fa-bolt']);
+        Menu::create(['name' => 'horizon', 'display_name' => 'Horizon', 'description' => '', 'icon' => 'fa fa-gears']);
+        Menu::create(['name' => 'admin/users', 'display_name' => 'Users', 'description' => '', 'icon' => 'fa fa-users']);
+        Menu::create(['name' => 'admin/roles', 'display_name' => 'Roles', 'description' => '', 'icon' => 'fa fa-unlock']);
+        Menu::create(['name' => 'admin/permissions', 'display_name' => 'Permissions', 'description' => '', 'icon' => 'fa fa-key']);
         Menu::create(['name' => 'admin/menus', 'display_name' => 'Menus', 'description' => '']);
         Menu::create(['name' => 'admin/employees', 'display_name' => 'Employees', 'description' => '']);
         Menu::create(['name' => 'admin/positions', 'display_name' => 'Positions', 'description' => '']);
