3cdf8d468ba33dd7d77640f49f2b1778cabbbb24
Yismen Jorge <yismen.jorge@gmail.com>
2019-05-28 09:16:03 -0400

    Fix bug where employees were not exporting passports
    Changed to query instead of view export to allow for mapping

--- app/Exports/Employees.php
+++ app/Exports/Employees.php
@@ -4,11 +4,16 @@ namespace App\Exports;
 
 use App\Employee;
 use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 
-class Employees implements FromView, WithTitle, ShouldAutoSize
+class Employees implements FromQuery, WithTitle, ShouldAutoSize, WithColumnFormatting, WithMapping, WithHeadings
 {
     protected $scope;
 
@@ -21,19 +26,59 @@ class Employees implements FromView, WithTitle, ShouldAutoSize
      *
      * @return Excel file
      */
-    public function view(): View
+    public function query()
     {
         $status = $this->scope;
-        $employees =  Employee::orderBy('first_name')
-            ->with('bankAccount')
-            ->$status()
-            ->get();
 
-        return view('employees.excel.employees', compact('employees'));
+        return Employee::query()
+            ->orderBy('first_name')
+            ->with('bankAccount')
+            ->$status();
     }
 
     public function title(): string
     {
         return "Employees";
+    }
+
+    public function map($employee): array
+    {
+        return [
+            $employee->id,
+            $employee->first_name,
+            $employee->second_first_name,
+            $employee->last_name,
+            $employee->second_last_name,
+            Date::dateTimeToExcel($employee->hire_date),
+            $employee->personal_id,
+            $employee->passport,
+            $employee->cellphone_number,
+            optional($employee->bankAccount)->account_number
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'F' => NumberFormat::FORMAT_DATE_XLSX15,
+            'G' => NumberFormat::FORMAT_TEXT,
+            'J' => NumberFormat::FORMAT_NUMBER,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'first_name',
+            'second_first_name',
+            'last_name',
+            'second_last_name',
+            'hire_date',
+            'personal_id',
+            'passport',
+            'cellphone_number',
+            'account_number',
+        ];
     }
 }
