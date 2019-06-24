290e1ef7a8ca1277c8b99df6cdee138c41732469
Yismen Jorge <yismen.jorge@gmail.com>
2019-06-24 08:45:39 -0400

    Add background image to the main header, Hide links to unused notes

--- resources/views/layouts/partials/main-sidebar.blade.php
+++ resources/views/layouts/partials/main-sidebar.blade.php
@@ -49,12 +49,12 @@
                     <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                    <li>
+                    {{-- <li>
                         <a href="{{ route('notes.index') }}"><i class="fa fa-circle-o text-red"></i> Notes List</a>
                     </li>
                     <li>
                         <a href="{{ route('admin.notes.index') }}"><i class="fa fa-circle-o text-red"></i> Admin Notes</a>
-                    </li>
+                    </li> --}}
                     <li>
                         <a href="{{ route('date_calc.index') }}"><i class="fa fa-circle-o text-red"></i> Date Diff
                             Calculator</a>
