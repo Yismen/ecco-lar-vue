505c911cf50b7b48a85fef8561fd0f605aca85b7
Yismen Jorge <yismen.jorge@gmail.com>
2019-07-05 13:40:55 -0400

    UI Improvements Supervisors

--- resources/views/supervisors/_free_employees.blade.php
+++ resources/views/supervisors/_free_employees.blade.php
@@ -1,29 +1,26 @@
-@if ($free_employees->count() > 0)
-    <div class="row">
-        <div class="box box-danger">
-            <div class="box-header">
-                <h4>
-                    List of Employees Not Assigned to Any Supervisor
-                    <span class="badge bg-yellow">{{ $free_employees->count() }}</span>
-                </h4>
-            </div>
-            <div class="box-body">
-                <table class="table table-condensed table-hover">
-                    <tbody>
-                        @foreach ($free_employees as $employee)
-                            <tr class="col-sm-6">
-                                <td>
-                                    <employee-check-box :employee="{{ $employee }}"
-                                    >,
-                                        {{ optional($employee->position)->name }}
-                                    </employee-check-box>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+<div class="col-sm-10 col-sm-offset-1">
+    <div class="box box-danger">
+        <div class="box-header">
+            <h4>
+                List of Employees Not Assigned to Any Supervisor
+                <span class="badge bg-yellow">{{ $free_employees->count() }}</span>
+            </h4>
+        </div>
+        <div class="box-body">
+            <table class="table table-condensed table-hover">
+                <tbody>
+                    @foreach ($free_employees as $employee)
+                        <tr class="col-sm-6">
+                            <td>
+                                <employee-check-box :employee="{{ $employee }}"
+                                >,
+                                    {{ optional($employee->position)->name }}
+                                </employee-check-box>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
-@endif
-
+</div>
--- resources/views/supervisors/index.blade.php
+++ resources/views/supervisors/index.blade.php
@@ -11,26 +11,34 @@
                         Supervisors List
                         <a href="/admin/supervisors/create" class="pull-right"><i class="fa fa-plus"></i> Add</a>
                     </h3>
-
-                    @include('supervisors._free_employees')
                 </div>
 
+            </div>
+
+            @if ($free_employees->count() > 0)
+                <div class="row">
+                    @include('supervisors._free_employees')
+                </div>
+            @endif
+
+            <div class="row">
                 <div class="col-sm-12">
                     @foreach ($supervisors as $supervisor)
                         @include('supervisors._list_with_employees')
                     @endforeach
                 </div>
-
             </div>
-                @if ($inactive_supervisors->count() > 0)
-                    <div class="row">
+
+            @if ($inactive_supervisors->count() > 0)
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
                         <div class="box box-danger">
                             <div class="box-header">
                                 <h4>List of Supervisors Inactives</h4>
                             </div>
                             <div class="box-body">
                                 @foreach ($inactive_supervisors as $supervisor)
-                                    <div class="col-lg-3 col-md-4 col-sm-6">
+                                    <div class="col-lg-3 col-md-4 col-xs-6">
                                         <a href="{{ route('admin.supervisors.edit', $supervisor->id) }}" class="text-warning">
                                             {{ $supervisor->name }}
                                             <i class="fa fa-pencil"></i>
@@ -40,16 +48,20 @@
                             </div>
                         </div>
                     </div>
-                @endif
-            <div class="col-sm-3 col-xs-7" style="position: fixed; bottom: 35%; right: 30px; padding: 15px ;  background-color: whitesmoke; border: darkgray; border-style: solid; border-width: thin;">
-                <div class="input-group">
-                    {{ Form::select('supervisor', $supervisors->pluck('name', 'id'), null, ['class' => 'form-control']) }}
-                    <span class="input-group-btn">
-                        <button type="submit" class="btn btn-warning">Re-Assign</button>
-                    </span>
                 </div>
+            @endif
 
-                @include('layouts.partials.errors')
+            <div class="row">
+                <div class="col-sm-3 col-xs-7" style="position: fixed; bottom: 35%; right: 30px; padding: 15px ;  background-color: whitesmoke; border: darkgray; border-style: solid; border-width: thin;">
+                    <div class="input-group">
+                        {{ Form::select('supervisor', $supervisors->pluck('name', 'id'), null, ['class' => 'form-control']) }}
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-warning">Re-Assign</button>
+                        </span>
+                    </div>
+
+                    @include('layouts.partials.errors')
+                </div>
             </div>
         </form>
 	</div>
