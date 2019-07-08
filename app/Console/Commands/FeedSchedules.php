2e330ec02906a42c1dda3263c896c934f1b14335
Yismen Jorge <yismen.jorge@gmail.com>
2019-07-08 15:41:01 -0400

    Implement arguments and option validations

--- app/Console/Commands/FeedSchedules.php
+++ app/Console/Commands/FeedSchedules.php
@@ -8,6 +8,10 @@ use Illuminate\Console\Command;
 
 class FeedSchedules extends Command
 {
+    protected $start_day;
+    protected $end_day;
+    protected $hours;
+
     /**
      * The name and signature of the console command.
      *
@@ -43,17 +47,25 @@ class FeedSchedules extends Command
      */
     public function handle()
     {
+        $this->start_day = (int) $this->argument('start-day');
+        $this->end_day = (int) $this->argument('end-day');
+        $this->hours = (int) $this->option('hours');
+
+        if (!$this->validate()) {
+            return $this->error('Invalid arguments. Must pass only integers. Start and End days correspond to days of the week, where 1 = Monday and 7 = Sunday. Hours corresponds to the amount of hours per shift, min 1, max 10.');
+        }
+
         $collection = Employee::actives()->whereDoesntHave('schedules')->get();
 
         $bar = $this->output->createProgressBar($collection->count());
         $bar->start();
 
         foreach ($collection as $row) {
-            for ($i = $this->argument('start-day'); $i <= $this->argument('end-day'); ++$i) {
+            for ($i = $this->start_day; $i <= $this->end_day; ++$i) {
                 $day = $this->days[$i - 1];
 
                 $schedule = (new Schedule())->createFor(
-                    $row->id, $day, $this->option('hours')
+                    $row->id, $day, $this->hours
                 );
             }
 
@@ -67,13 +79,13 @@ class FeedSchedules extends Command
 
     private function validate()
     {
-        if ($this->argument('start-day') < 1
-            || $this->argument('start-day') > 7
-            || $this->argument('end-day') < 1
-            || $this->argument('end-day') > 7
-            || $this->option('hours') < 0
-            || $this->option('hours') > 9
-            || $this->argument('end-day')) {
+        if ($this->start_day < 1
+            || $this->start_day > 7
+            || $this->end_day < 1
+            || $this->end_day > 7
+            || $this->hours < 1
+            || $this->hours > 10
+            || $this->end_day < $this->start_day) {
             return false;
         }
 
