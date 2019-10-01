a19c2939d17a005c5a74027997b20e92ee1b82f6
Yismen Jorge <yismen.jorge@gmail.com>
2019-10-01 08:09:16 -0400

    Improve style in Your Apps in app view

--- resources/views/app/partials/your-apps.blade.php
+++ resources/views/app/partials/your-apps.blade.php
@@ -3,13 +3,13 @@
 <div style="display: flex; justify-content: space-between; flex-wrap: wrap;">
     @if ($user && $user->roles->count() > 0)
         @foreach ($user->roles as $role)
-            <div class="box box-primary" style="max-height: 200px; overflow-y: auto; width: 200px;">
+            <div class="box box-primary" style="max-height: 200px; overflow-y: auto; width: 200px; ">
                 <div class="box-header">
-                    <h4>{{ personName($role->name) }}</h4>
+                    <h5 class="no-margin" style="text-transform: uppercase;">{{ personName($role->name) }}</h5>
                 </div>
 
-                <div class="box-body">
-                    @if ($role->menus && $role->menus->count() > 0)
+                @if ($role->menus && $role->menus->count() > 0)
+                    <div class="box-body" style="padding-top: 0;">
                         <div class="list-group">
                             @foreach ($role->menus as $menu)
                                 <a href="/{{ $menu->name }}" class="list-group-item" style="padding: 3px 10px;">
@@ -17,8 +17,8 @@
                                 </a>
                             @endforeach
                         </div>
-                    @endif
-                </div>
+                    </div>
+                @endif
             </div>
         @endforeach
     @endif
