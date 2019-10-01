ab62ce61c4f960df5d9fa1368e27d73c02f272d3
Yismen Jorge <yismen.jorge@gmail.com>
2019-10-01 07:45:51 -0400

    Change layout option in app landing page

--- resources/views/app/partials/stats.blade.php
+++ resources/views/app/partials/stats.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-xs-6 col-md-4 col-lg-3">
+    <div class="col-xs-6 col-md-4 col-xl-3">
         @component('components.info-box', ['color' => 'bg-red', 'icon' => 'fa fa-users'])
             Users
             @slot('number')
@@ -8,7 +8,7 @@
         @endcomponent
     </div>
 
-    <div class="col-xs-6 col-md-4 col-lg-3">
+    <div class="col-xs-6 col-md-4 col-xl-3">
         @component('components.info-box', ['color' => 'bg-red', 'icon' => 'fa fa-building'])
             Sites
             @slot('number')
@@ -17,7 +17,7 @@
         @endcomponent
     </div>
 
-    <div class="col-xs-6 col-md-4 col-lg-3">
+    <div class="col-xs-6 col-md-4 col-xl-3">
         @component('components.info-box', ['color' => 'bg-red', 'icon' => 'fa fa-book'])
             Projects
             @slot('number')
@@ -26,7 +26,7 @@
         @endcomponent
     </div>
 
-    <div class="col-xs-6 col-md-4 col-lg-3">
+    <div class="col-xs-6 col-md-4 col-xl-3">
         @component('components.info-box', ['color' => 'bg-red', 'icon' => 'fa fa-users'])
             Employees
             @slot('number')
