b32a4b4370592d584dcaa6fca9131ac5da080280
Yismen Jorge <yismen.jorge@gmail.com>
2019-10-01 22:36:36 -0400

    Improve your-apps design in apps view

--- resources/views/app/partials/your-apps.blade.php
+++ resources/views/app/partials/your-apps.blade.php
@@ -1,25 +1,27 @@
 <h4>Your Apps</h4>
 
-<div style="display: flex; justify-content: space-between; flex-wrap: wrap;">
-    @if ($user && $user->roles->count() > 0)
+@if ($user && $user->roles->count() > 0)
+    <div class="row">
         @foreach ($user->roles as $role)
-            <div class="box box-primary" style="max-height: 200px; overflow-y: auto; width: 200px; ">
-                <div class="box-header">
-                    <h5 class="no-margin" style="text-transform: uppercase;">{{ personName($role->name) }}</h5>
-                </div>
-
-                @if ($role->menus && $role->menus->count() > 0)
-                    <div class="box-body" style="padding-top: 0;">
-                        <div class="list-group">
-                            @foreach ($role->menus as $menu)
-                                <a href="/{{ $menu->name }}" class="list-group-item" style="padding: 3px 10px;">
-                                    {{ $menu->display_name }}
-                                </a>
-                            @endforeach
-                        </div>
+            <div class="col-xs-6 col-md-12" style="">
+                <div class="box box-primary" style="max-height: 200px; overflow-y: auto; ">
+                    <div class="box-header">
+                        <h5 class="no-margin" style="text-transform: uppercase;">{{ personName($role->name) }}</h5>
                     </div>
-                @endif
+
+                    @if ($role->menus && $role->menus->count() > 0)
+                        <div class="box-body no-padding" style="padding-top: 0;">
+                            <div class="list-group no-margin">
+                                @foreach ($role->menus as $menu)
+                                    <a href="/{{ $menu->name }}" class="list-group-item" style="padding: 3px 10px;">
+                                        {{ $menu->display_name }}
+                                    </a>
+                                @endforeach
+                            </div>
+                        </div>
+                    @endif
+                </div>
             </div>
         @endforeach
-    @endif
-</div>
+    </div>
+@endif
