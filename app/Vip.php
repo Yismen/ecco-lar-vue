145fc7fce7a158f12f990f8ba3da5598e858191d
Yismen Jorge <yismen.jorge@gmail.com>
2019-10-04 21:47:26 -0400

    Add documentation

--- app/Vip.php
+++ app/Vip.php
@@ -10,6 +10,11 @@ class Vip extends Model
 
     protected $fillable = ['employee_id', 'since'];
 
+    /**
+     * A vip belongs to an employee.
+     *
+     * @return relation one to one
+     */
     public function employee()
     {
         return $this->belongsTo(Employee::class);
