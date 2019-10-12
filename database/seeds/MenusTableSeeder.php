b0dc910bf969f77805915db3f6d079d014b64928
Yismen Jorge <yismen.jorge@gmail.com>
2019-10-12 18:03:06 -0400

    Change initial menut to log-viewer

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -14,7 +14,7 @@ class MenusTableSeeder extends Seeder
     {
 
         Menu::create(['name' => 'telescope', 'display_name' => 'Telescope', 'description' => '', 'icon' => 'fa fa-bolt']);
-        Menu::create(['name' => 'horizon', 'display_name' => 'Horizon', 'description' => '', 'icon' => 'fa fa-gears']);
+        Menu::create(['name' => 'log-viewer', 'display_name' => 'Log Viewer', 'description' => '', 'icon' => 'fa fa-archive']);
         Menu::create(['name' => 'admin/users', 'display_name' => 'Users', 'description' => '', 'icon' => 'fa fa-users']);
         Menu::create(['name' => 'admin/roles', 'display_name' => 'Roles', 'description' => '', 'icon' => 'fa fa-unlock']);
         Menu::create(['name' => 'admin/permissions', 'display_name' => 'Permissions', 'description' => '', 'icon' => 'fa fa-key']);
