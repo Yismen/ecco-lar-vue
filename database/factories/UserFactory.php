ae2bb147e4d74ea790484f1f0ad50eb1f8b8e28e
Yismen Jorge <yismen.jorge@gmail.com>
2019-10-12 00:20:12 -0400

    Change default password signature

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,12 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'email' => $faker->unique()->safeEmail,
+        'password' => bcrypt('secret'),
+        'remember_token' => Str::random(10),
     ];
 });
