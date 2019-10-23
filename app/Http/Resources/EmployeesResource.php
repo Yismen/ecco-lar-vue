0fa64444011633e8a5a3719629fa1fcd92d8a7ef
Yismen Jorge <yismen.jorge@gmail.com>
2019-10-23 10:18:34 -0400

    Add api endpoint for recent employees

---
+++ app/Http/Resources/EmployeesResource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeesResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "first_name" => $this->first_name,
+            "second_first_name" => $this->second_first_name,
+            "last_name" => $this->last_name,
+            "second_last_name" => $this->second_last_name,
+            "full_name" => $this->full_name,
+            "hire_date" => $this->hire_date->format('Y-m-d'),
+            "personal_id" => $this->personal_id,
+            "passport" => $this->passport,
+            "date_of_birth" => $this->date_of_birth->format('Y-m-d'),
+            "cellphone_number" => $this->cellphone_number,
+            "secondary_phone" => $this->secondary_phone,
+            "site" => optional($this->site)->name,
+            "project" => optional($this->project)->name,
+            "position" => optional($this->position)->name,
+            "department" => optional(optional($this->position)->department)->name,
+            "supervisor" => optional($this->supervisor)->name,
+            "gender" => optional($this->gender)->name,
+            "marital" => optional($this->marital)->name,
+            "ars" => optional($this->ars)->name,
+            "afp" => optional($this->afp)->name,
+            "nationality" => optional($this->nationality)->name,
+            "has_kids" => $this->has_kids,
+            "photo" => $this->photo,
+            "active" => $this->active,
+            "status" => $this->status,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -22,18 +22,23 @@ Route::middleware('auth:api')->group(function () {
     Route::resource('departments', 'DepartmentsController');
     Route::resource('nationalities', 'NationalitiesController')->only('store');
     Route::resource('payment_frequencies', 'PaymentFrequenciesController');
+    
+    Route::get('employees', 'Api\EmployeeController@index');
+    Route::get('employees/all', 'Api\EmployeeController@index');
+    Route::get('employees/actives', 'Api\EmployeeController@actives');
+    Route::get('employees/recents', 'Api\EmployeeController@recents');
 
     Route::get('performances/campaigns', 'Api\Performances\CampaignsController@list');
     Route::get('performances/downtimes', 'Api\Performances\PerformancesController@downtimes');
     Route::get('performances/downtime_reasons', 'Api\Performances\DowntimesController@reasons');
     Route::get('performances/employees', 'Api\Performances\DowntimesController@employees');
     Route::get('performances/login_names', 'Api\Performances\EmployeesController@loginNames');
-    Route::get('performances/performance_data/last/{many}/months', 'Api\Performances\PerformancesController@data');
     Route::get('performances/projects', 'Api\Performances\ProjectsController@list');
     Route::get('performances/schedules', 'Api\Performances\EmployeesController@schedules');
     Route::get('performances/sites', 'Api\Performances\SitesController@list');
     Route::get('performances/supervisors', 'Api\Performances\SupervisorsController@list');
     Route::get('performances/supervisors/actives', 'Api\Performances\SupervisorsController@actives');
-
+    
+    Route::get('performances/performance_data/last/{many}/months', 'Api\Performances\PerformancesController@data');
     Route::get('/blackhawk/de/management', 'Blackhawk\DE\ManagementController@dashboardData');
 });
