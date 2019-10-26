f5a58293809bd764a01fe15afbcab8d262458cda
= <=>
2019-10-26 06:22:37 -0400

    Remove withoutOverlaping option

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,19 +30,19 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('telescope:prune --hours=24')->dailyAt('23:40')->withoutOverlapping()->timezone('America/New_York');
+        $schedule->command('telescope:prune --hours=24')->dailyAt('06:40')->timezone('America/New_York');
 
-        $schedule->command('dainsys:feed-shifts --hours=7.5 --saturday=1')->dailyAt('14:59')->withoutOverlapping()->timezone('America/New_York');
-        $schedule->command('dainsys:feed-schedules --days=15 --since-days-ago=0')->dailyAt('15:10')->withoutOverlapping()->timezone('America/New_York');
+        $schedule->command('dainsys:feed-shifts --hours=7.5 --saturday=1')->dailyAt('14:59')->timezone('America/New_York');
+        $schedule->command('dainsys:feed-schedules --days=15 --since-days-ago=0')->dailyAt('15:10')->timezone('America/New_York');
 
-        $schedule->command('dainsys:employees-hired --months=1')->weeklyOn(2, '15:59')->withoutOverlapping()->timezone('America/New_York');
-        $schedule->command('dainsys:employees-hired --months=1')->weeklyOn(5, '15:59')->withoutOverlapping()->timezone('America/New_York');
-        $schedule->command('dainsys:employees-terminated --months=1')->weeklyOn(2, '15:59')->withoutOverlapping()->timezone('America/New_York');
-        $schedule->command('dainsys:employees-terminated --months=1')->weeklyOn(5, '15:59')->withoutOverlapping()->timezone('America/New_York');
+        $schedule->command('dainsys:employees-hired --months=1')->weeklyOn(2, '15:59')->timezone('America/New_York');
+        $schedule->command('dainsys:employees-hired --months=1')->weeklyOn(5, '15:59')->timezone('America/New_York');
+        $schedule->command('dainsys:employees-terminated --months=1')->weeklyOn(2, '15:59')->timezone('America/New_York');
+        $schedule->command('dainsys:employees-terminated --months=1')->weeklyOn(5, '15:59')->timezone('America/New_York');
 
-        $schedule->command('dainsys:capillus-flash')->twiceDaily(0, 3)->withoutOverlapping()->timezone('America/New_York');
-        $schedule->command('dainsys:capillus-flash')->twiceDaily(6, 9)->withoutOverlapping()->timezone('America/New_York');
-        $schedule->command('dainsys:capillus-flash')->twiceDaily(12, 15)->withoutOverlapping()->timezone('America/New_York');
-        $schedule->command('dainsys:capillus-flash')->twiceDaily(18, 21)->withoutOverlapping()->timezone('America/New_York');
+        $schedule->command('dainsys:capillus-flash')->twiceDaily(0, 3)->timezone('America/New_York');
+        $schedule->command('dainsys:capillus-flash')->twiceDaily(6, 9)->timezone('America/New_York');
+        $schedule->command('dainsys:capillus-flash')->twiceDaily(12, 15)->timezone('America/New_York');
+        $schedule->command('dainsys:capillus-flash')->twiceDaily(18, 21)->timezone('America/New_York');
     }
 }
