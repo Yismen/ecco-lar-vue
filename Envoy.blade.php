4b9ac72387f1885376133f68fcce0203bd951875
Yismen Jorge <yismen.jorge@gmail.com>
2019-12-10 14:06:45 -0400

    Add Envoy file

---
+++ Envoy.blade.php
@@ -0,0 +1,9 @@
+@servers(['web' => ['yjorge@192.168.10.24']])
+
+@task('deploy', ['on' => 'web'])
+    cd /var/www/html/dainsys
+    git pull origin prod
+    composer install --no-dev
+    php artisan migrate --force
+    php artisan optimize
+@endtask
