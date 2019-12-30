06001233559e574987f02c498f88b3aeb3d5684b
Yismen Jorge <yismen.jorge@gmail.com>
2019-12-30 15:12:39 -0400

    Update deploy script
    Still no 0 downtime

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,28 @@
-@servers(['web' => ['root@192.168.10.24']])
+@servers(['local' => ['127.0.0.1'], 'web' => ['root@192.168.10.24']])
 
-@task('deploy', ['on' => 'web', 'confirm' => true])
+@story('deploy')
+    checkout
+    merge
+    push
+    cmaster
+    serve
+@endstory
+
+@task('checkout', ['on' => 'local'])
+    git checkout prod
+@endtask
+@task('merge', ['on' => 'local'])
+    git merge master
+@endtask
+@task('push', ['on' => 'local'])
+    git push origin prod
+@endtask
+@task('cmaster', ['on' => 'local'])
+    git checkout master
+@endtask
+
+
+@task('serve', ['on' => 'web'])
     cd /var/www/html/dainsys
     git checkout prod
     git pull origin prod
