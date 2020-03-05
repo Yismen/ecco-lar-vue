809f861feb8d425bafd0186a7e4615c7f3f91ae8
Yismen Jorge <yismen.jorge@gmail.com>
2020-03-05 12:55:53 -0400

    Add test for political commands

---
+++ tests/Unit/PoliticalCommandsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Mail\Political\PoliticalFlashMail;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
+
+class PoliticalCommandsTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /** @test */
+    public function it_sends_the_political_flash_report()
+    {
+        Mail::fake();
+        
+        $this->artisan('dainsys:political-send-hourly-flash')
+            ->expectsOutput('Political Hourly Flash sent!')
+            ->assertExitCode(0);
+    
+        Mail::assertSent(PoliticalFlashMail::class);
+    }
+}
