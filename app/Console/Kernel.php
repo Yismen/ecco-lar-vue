303a2f95ef35c93cce82ad7508d1a7ea6450c7a1
Yismen Jorge <yismen.jorge@gmail.com>
2020-03-08 03:31:31 -0400

    Change runtime to 3am

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -82,8 +82,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('dainsys:capillus-send-agent-call-data-dump-report')->dailyAt('06:10')->timezone('America/New_York'); 
         $schedule->command('dainsys:capillus-send-daily-leads-report')->dailyAt('06:15')->timezone('America/New_York');
 
-        $schedule->command('dainsys:political-send-hourly-flash')->hourly()->timezone('America/New_York');
-        $schedule->command('dainsys:capillus-send-daily-leads-report')->dailyAt('04:00')->timezone('America/New_York');
+        $schedule->command('dainsys:capillus-send-daily-leads-report')->dailyAt('03:00')->timezone('America/New_York');
         
+        $schedule->command('dainsys:political-send-hourly-flash')->hourly()->timezone('America/New_York');
     }
 }
