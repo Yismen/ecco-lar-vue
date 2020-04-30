352d1fd60de756bc7b5a8e7da0b99185f8945199
Yismen Jorge <yismen.jorge@gmail.com>
2020-04-30 16:55:38 -0400

    Improve factory feature
    Leverage decision of whether to create a new one or not to Laravel

--- database/factories/PerformanceFactory.php
+++ database/factories/PerformanceFactory.php
@@ -2,11 +2,11 @@
 
 use Carbon\Carbon;
 use Faker\Generator as Faker;
+
 $factory->define(App\Performance::class, function (Faker $faker) {
     $date = Carbon::now();
     $employee = factory(App\Employee::class)->create();
     $campaign = factory(App\Campaign::class)->create();
-    $supervisor = factory(App\Supervisor::class)->create();
 
     return [
         'unique_id' => "{$date->format('Y-m-d')}-{$employee->id}-{$campaign->id}",
@@ -14,7 +14,7 @@ $factory->define(App\Performance::class, function (Faker $faker) {
         'employee_id' => $employee->id,
         'name' => $employee->fullName,
         'campaign_id' => $campaign->id,
-        'supervisor_id' => $supervisor->id,
+        'supervisor_id' => factory(App\Supervisor::class),
         'sph_goal' => 10,
         'login_time' => 8,
         'production_time' => 7.5,
