5d727d268968fbe09c21a9e4c9416202c18a6712
Yismen Jorge <yismen.jorge@gmail.com>
2020-04-30 16:52:25 -0400

    Add unique fag

--- database/factories/NationalityFactory.php
+++ database/factories/NationalityFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Nationality::class, function (Faker $faker) {
     return [
-        'name' => $faker->country . "_" . $faker->randomDigit,
+        'name' => $faker->unique()->country . "_" . $faker->randomNumber(),
     ];
 });
