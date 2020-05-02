9f38692675320ea4227bc7d08652d800adb40197
Yismen Jorge <yismen.jorge@gmail.com>
2020-05-02 12:37:21 -0400

    Fix route name conflicts

--- routes/Web/Auth/blackhawk-cs.php
+++ routes/Web/Auth/blackhawk-cs.php
@@ -3,12 +3,12 @@
 Route::get('bhcs_manager', 'BlackHawkCS\ManagementController@index');
 Route::get('blackhawk_cs/api/dashboard/management', 'BlackHawkCS\ManagementController@dashboard');
 
-Route::get('bhcs_statistic', 'BlackHawkCS\Statistic\ImportController@index');
+// Route::get('bhcs_statistic', 'BlackHawkCS\Statistic\ImportController@index');
 
-Route::post('bhcs_statistic/import/qa', 'BlackHawkCS\Statistic\ImportController@qa')->name('import-qa');
-Route::post('bhcs_statistic/import/qa-errors', 'BlackHawkCS\Statistic\ImportController@qaErrors')->name('import-qa-errors');
-Route::post('bhcs_statistic/import/lob', 'BlackHawkCS\Statistic\ImportController@lob')->name('import-lob-summary');
-Route::post('bhcs_statistic/import/performance', 'BlackHawkCS\Statistic\ImportController@performance')->name('import-performance');
+// Route::post('bhcs_statistic/import/qa', 'BlackHawkCS\Statistic\ImportController@qa')->name('import-qa');
+// Route::post('bhcs_statistic/import/qa-errors', 'BlackHawkCS\Statistic\ImportController@qaErrors')->name('import-qa-errors');
+// Route::post('bhcs_statistic/import/lob', 'BlackHawkCS\Statistic\ImportController@lob')->name('import-lob-summary');
+// Route::post('bhcs_statistic/import/performance', 'BlackHawkCS\Statistic\ImportController@performance')->name('import-performance');
 
 Route::get('bhcs_supervisor', 'BlackHawkCS\SupervisorController@index');
 Route::get('blackhawk_cs/api/dashboard/supervisor', 'BlackHawkCS\SupervisorController@dashboard');
--- routes/Web/Auth/escal_records.php
+++ routes/Web/Auth/escal_records.php
@@ -5,4 +5,4 @@ Route::resource('escalations_records', 'EscalRecordsController', ['except' => ['
 Route::get('api/escalations_records/clients', 'EscalRecordsController@getClients');
 Route::post('escalations_records/search', 'EscalRecordsController@search');
 Route::post('api/escalations_records/search', 'EscalRecordsController@search');
-Route::resource('api/escalations_records', 'EscalRecordsController', ['except' => ['show']]);
+Route::resource('api/escalations_records_api', 'EscalRecordsController', ['except' => ['show']]);
