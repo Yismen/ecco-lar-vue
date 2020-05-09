df0c2d02ca6e78671a8e8ba0124405a0526e1f23
Yismen Jorge <yismen.jorge@gmail.com>
2020-05-09 06:09:20 -0400

    Add users search box

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -49,51 +49,14 @@ the users configurations and setting.']) @section('content')
                         </a>
                     </h3>
                     <div class="row">
-                        @foreach ($users as $user)
-                            <div class="col-sm-6">
-                                <div class="box box-primary info-box bg-grey">
-                                    <span class="info-box-icon">
-                                        @if (file_exists(optional($user->profile)->photo))
-                                            <a href="{{ asset($user->profile->photo) }}" target="_user_photo">
-                                                <img src="{{ asset($user->profile->photo) }}"
-                                                    class="profile-user-img img-responsive img-circle img-thumbnail" alt="Image"
-                                                >
-                                            </a>
-                                        @else
-                                            <img src="http://placehold.it/300x300"
-                                                class="profile-user-img img-responsive img-circle img-thumbnail" alt="Image"
-                                            >
-                                        @endif
-                                    </span>
-
-                                    <div class="info-box-content">
-                                        <span class="info-box-text">
-                                            <a href="{{ route('admin.users.show', $user->id) }}">
-                                                <i class="fa fa-user"></i>
-                                                {{ $user->name }} 
-                                            </a>
-                                            <i 
-                                                class="fa fa-circle {{ $user->isOnline() ? 'text-green' : 'text-gray'}}"
-                                                title="{{ $user->isOnline() ? 'Online' : 'Away'}}"
-                                            ></i>
-                                            <a href="{{ route('admin.users.edit', $user->id) }}" class="pull-right">
-                                                <i class="fa fa-pencil"></i>
-                                            </a>
-                                        </span>
-
-                                        {{-- <span class="info-box-number">41,410</span> --}}
-                                         @if (count($user->roles) > 0)
-                                            <strong>Roles:</strong>
-                                            @foreach ($user->roles as $role)
-                                                <span class="label label-primary">{{ ucwords($role->name) }}</span>
-                                            @endforeach
-                                        @endif
-                                    </div>
-                                    <!-- /.info-box-content -->
-                                </div>
-                                <!-- /.info-box -->
-                            </div>
-                        @endforeach
+                        <div class="form-group">
+                            Search: <input type="text" id="js-search">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div id="js-results">
+                            @include('partials._users')
+                        </div>
                     </div>
 
                 </div>
@@ -131,5 +94,14 @@ the users configurations and setting.']) @section('content')
 </div>
 @stop
 @section('scripts')
-
+    <script defer>
+        (function() {
+            document.getElementById('js-search')
+                .addEventListener('input', _.debounce(function(e) {
+                    fetch(`/admin/users/search?q=${e.target.value}`)
+                        .then(response => response.text())
+                        .then(html => document.getElementById('js-results').innerHTML = html)
+                }, 700))
+        })()
+    </script>
 @stop
---
+++ routes/Web/Auth/partials.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::get('users/search', 'Partials\UserController');
